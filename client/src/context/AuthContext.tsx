d638dd4c02533a1f6d7d8cf27dab9147952d742a
Henrique Moreira <riqimoreiraa@gmail.com>
2022-07-28 11:23:42 -0300

    types, handle register

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,18 +1,47 @@
 import React, { createContext, useState, useEffect, ReactNode } from 'react';
 import axios from 'axios';
+import api from '../api';
 
 type FormProviderProps = {
     children: ReactNode;
 }
 
+type UserForm = {
+    name: string;
+    email: string;
+    password: string;
+    password2: string   
+}
+
+type UserData = {
+    _id: string;
+    name: string;
+    email: string;
+    user_img: string;
+    token: string
+}
+
 const Context = createContext<any | undefined>(undefined);
 
 function AuthContext({ children }: FormProviderProps) {
+    const [authenticated, setAuthenticated] = useState<boolean>(false) 
+    const [userForm, setUserForm] = useState<UserForm>({ name: '', email: '', password: '', password2: '' })
 
+    const { name, email, password } = userForm
 
+    const handleRegister = async () => {
+        const { data } = await api.post<UserData>('/users/register', {
+            name,
+            email,
+            password,
+        })
+
+        localStorage.setItem('token', JSON.stringify(data.token))
+        api.defaults.headers.Authorization = `Bearer ${data.token}`
+    }
 
   return (
-    <Context.Provider value={{  }}>
+    <Context.Provider value={{ userForm, setUserForm }}>
         { children }
     </Context.Provider>
   )
